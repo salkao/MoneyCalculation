f14bfba05f2b2bff991e46bce7153359419646ba
salkao <salalex48@gmail.com>
2025-05-10 10:13:55 +0200

    use singe quoutes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
-import type { RouteRecordRaw } from "vue-router";
-import Home from "../components/views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+import Home from '@/components/views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  { path: "/", name: "Home", component: Home },
+  { path: '/', name: 'Home', component: Home },
 ];
 
 const router = createRouter({
