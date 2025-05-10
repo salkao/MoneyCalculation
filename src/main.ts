bb8452dfcc80f5f9dd87a29d8a7324e34477d106
salkao <salalex48@gmail.com>
2025-05-10 14:04:16 +0200

    Add SCSS support to Vite configuration, create main SCSS file, and define variables for styling.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import router from './router';
-
+import './assets/scss/app.scss';
 const app = createApp(App);
 
 app.use(router);
