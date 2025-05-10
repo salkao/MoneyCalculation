6b2dcecaacc83db1834d79143b2de8c0746979a4
salkao <salalex48@gmail.com>
2025-05-10 19:59:37 +0200

    Add MbButton component with customizable label and click event.

---
+++ src/components/elements/MbButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="onClick" class="mb-button">
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['click']);
+
+function onClick() {
+  emit('click');
+}
+</script>
+
+<style lang="scss">
+.mb-button {
+  background-color: $button-primary-color;
+  color: $primary-color;
+  font-size: 21px;
+  border: none;
+  border-radius: 4px;
+  width: 100%;
+  padding: 12px 0;
+  cursor: pointer;
+}
+</style>
