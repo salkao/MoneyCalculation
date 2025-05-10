9ceb16e97eab4dfb86cdb3d533e21ce703e98ee0
salkao <salalex48@gmail.com>
2025-05-10 10:31:06 +0200

    Add Welcome view component and update router to use it as the home page.

---
+++ src/components/views/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="welcome-container">
+    <h1 class="welcome-title">Welcome</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 import Home from '@/components/views/Home.vue';
-
+import Welcome from '@/components/views/Welcome.vue';
 const routes: Array<RouteRecordRaw> = [
-  { path: '/', name: 'Home', component: Home },
+  { path: '/', name: 'Welcome', component: Welcome },
+  { path: '/home', name: 'Home', component: Home },
 ];
 
 const router = createRouter({
