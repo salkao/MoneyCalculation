4b364aab56deb3103ddc2327ec2bba184ecaa003
salkao <salalex48@gmail.com>
2025-05-12 21:19:01 +0200

    Add calculation and optional expenses sections to Home component.

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -3,7 +3,26 @@
     <Header />
     <div class="home-content-container">
       <ListOfExpenses />
-      <div class="home-content-container-right"></div>
+      <div class="home-content-container-data">
+        <Box :title="'Calculation'" :content-title="'Income'">
+          <template #content>
+            <div class="box-content-data">
+              <h3>{{ user.income }}</h3>
+            </div>
+          </template>
+        </Box>
+        <Box :title="'Optionals'" :content-title="'choose any fix expenses'">
+          <template #content>
+            <div class="box-content-data">
+              <OptionalExpenseItem
+                v-for="expense in store.getOptionalExpenses"
+                :key="expense.id"
+                :expense="expense"
+              />
+            </div>
+          </template>
+        </Box>
+      </div>
     </div>
   </div>
   <div v-if="isModalOpen" class="modal">
@@ -18,11 +37,14 @@ import Header from '@/components/widgets/Header.vue';
 import ListOfExpenses from '@/components/widgets/ListOfExpenses.vue';
 import Expense from '@/components/views/Expense.vue';
 import { useBudgetStore } from '@/stores/budget';
+import Box from '@/components/widgets/Box.vue';
+import OptionalExpenseItem from '@/components/widgets/OptionalExpenseItem.vue';
 
 const store = useBudgetStore();
 
 const isModalOpen = computed(() => store.getIsModalOpen);
 const expense = computed(() => store.getEditingExpense);
+const user = computed(() => store.getUser);
 
 function handleSaveExpense(expense: Expense) {
   store.saveExpense({ id: expense.id || nanoid(), ...expense });
@@ -41,7 +63,11 @@ function handleSaveExpense(expense: Expense) {
 
   .home-content-container {
     display: flex;
-    flex-direction: column;
+    width: 100%;
+
+    .home-content-container-data {
+      display: flex;
+    }
   }
 }
 </style>
