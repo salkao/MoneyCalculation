0f40e72420f3819e09d683c9856583b6383bab11
salkao <salalex48@gmail.com>
2025-05-12 21:17:00 +0200

    Add Box component with template, script, and styles for reusable UI element.

---
+++ src/components/widgets/Box.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <h2 class="box-title">{{ title }}</h2>
+    <div class="box-content">
+      <h3>{{ contentTitle }}</h3>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  contentTitle: string;
+}>();
+</script>
+
+<style lang="scss">
+.box {
+  border-radius: 3px;
+  background-color: $box-color;
+  padding: 20px 30px;
+
+  .box-title {
+    margin: 0 auto;
+  }
+  .box-content {
+    background-color: $secondary-color;
+  }
+}
+</style>
